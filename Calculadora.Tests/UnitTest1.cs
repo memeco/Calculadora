9e7230a153847ab860333f687e119808dcffba04
Emerson Marques Pedro <emersonmp@gmail.com>
2024-07-11 19:01:25 -0300

    Mais testes

--- Calculadora.Tests/UnitTest1.cs
+++ Calculadora.Tests/UnitTest1.cs
@@ -13,5 +13,34 @@ public class CalculadoraBasicaTests
         Assert.Equal(8, resultado);
     }
 
-    // Rest of the test methods...
+    [Fact]
+    public void TestSubtracao()
+    {
+        var calculadora = new CalculadoraOperations();
+        var resultado = calculadora.Subtracao(5, 3);
+        Assert.Equal(2, resultado);
+    }
+
+    [Fact]
+    public void TestMultiplicacao()
+    {
+        var calculadora = new CalculadoraOperations();
+        var resultado = calculadora.Multiplicacao(5, 3);
+        Assert.Equal(15, resultado);
+    }
+
+    [Fact]
+    public void TestDivisao()
+    {
+        var calculadora = new CalculadoraOperations();
+        var resultado = calculadora.Divisao(5, 3);
+        Assert.Equal(1.6666666666666667, resultado, precision: 15); // Considerando ponto flutuante
+    }
+
+    [Fact]
+    public void TestDivisaoPorZero()
+    {
+        var calculadora = new CalculadoraOperations();
+        Assert.Throws<DivideByZeroException>(() => calculadora.Divisao(5, 0));
+    }
 }
